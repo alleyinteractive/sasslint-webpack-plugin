661fb507783b7fbfe80ca89058c004868a546505
Jackson Marketon <jmarketon@gmail.com>
2016-01-27 14:16:02 -0600

    Linter is working as expected

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ var assign = require('object-assign');
 // Modules
 var linter = require('./lib/linter');
 
-function apply(options, compiler, callback) {
+function apply(options, compiler) {
   // acces to compiler and options
-  linter(compiler.context + options.glob, options, callback);
+  compiler.plugin('compilation', function(c, params) {
+    var report = linter(compiler.context + options.glob, options);
+
+    c.plugin('seal', function() {
+      var _this = this;
+      report.forEach(function(x) {
+        if(x.error) {
+          _this.errors.push(x.file);
+        } else {
+          _this.warnings.push(x.file);
+        }
+      });
+    });
+  });
 }
 
-module.exports = function(options, callback) {
+module.exports = function(options) {
   options = options || {};
   options.glob = options.glob || '**/*.s?(c|a)ss';
 
